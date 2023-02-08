f997374b28884619dd46bfb31e3121e59d3587ac
Zain Raza <zainrazasocial@gmail.com>
2023-02-08 21:49:35 +0500

    env file and basic server done

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/goals", (req, res) => {
+    res.status(200).json({ message: "Hello Backend" })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+});
