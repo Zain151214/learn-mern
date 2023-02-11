9611c5133f3a4ea761c37ddc84f6266d4db5429d
Zain Raza <zainrazasocial@gmail.com>
2023-02-11 16:28:19 +0500

    express-async-handler added for handling exceptions

--- backEnd/controller/goals.js
+++ backEnd/controller/goals.js
@@ -1,20 +1,23 @@
+const asyncHandler = require("express-async-handler");
 
-const createGoal = (req, res) => {
+const createGoal = asyncHandler(async (req, res) => {
+    const goal = await req.body;
+    console.log(goal);
     res.status(201).json({ message: "Goal Created" })
-}
+})
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get Goals" });
-}
+})
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Goal Updated of id ${req.params.id}` })
 
-}
+})
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Goal Delated of id ${req.params.id}` })
-}
+})
 
 
 module.exports = { getGoals, createGoal, updateGoal, deleteGoal }
